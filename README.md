fdf004f960fa62d69cf666d36343ef56b7bad388
Lance Stout <lancestout@gmail.com>
2013-09-23 02:18:01 -0700

    Update readme, add tests

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # jxt: JSON/XML Translation for the Browser
 
+## What is this?
+
+A basic in-browser framework for translating XML to JSON and back again.
+
+Suitable for use with browserify/CommonJS on the client.
+
+If you're not using browserify or you want AMD support use `jxt.bundle.js`.
+
 
 ## Installing
 
@@ -7,8 +15,60 @@
 npm install jxt
 ```
 
-## Examples
+## How to use it
 
+First, we define the mapping between our XML and desired JSON:
+
+```js
+var Message = jxt.define({
+    name: 'message',
+    namespace: 'jabber:client',
+    element: 'message',
+    fields: {
+        to: jxt.attribute('to'),
+        from: jxt.attribute('from'),
+        subject: jxt.subText('jabber:client', 'subject'),
+        body: jxt.subText('jabber:client', 'body')
+    }
+});
+```
+
+Now, we can create `Message` objects, and set fields and treat it just like JSON, and it will map it to XML.
+
+```js
+var msg = new Message();
+msg.to = 'foo@example.com';
+msg.body = 'giving a demo of jxt';
+
+console.log(msg.toJSON());
+console.log(msg.toString());
+
+// {to: 'foo@example.com', body: 'giving a demo of jxt'}
+// <message xmlns="jabber:client" to="foo@example.com"><body>giving a demo of jxt</body></message>
+```
+
+Mappings can be extended:
+
+```js
+var Ext = jxt.define({
+    name: 'demoExt',
+    namespace: 'jxt',
+    element: 'demo',
+    fields: {
+        text: jxt.text()
+    }
+});
+
+jxt.extend(Message, Ext);
+
+var msg = new Message();
+msg.demoExt.text = 'an extension';
+
+console.log(msg.toJSON());
+console.log(msg.toString());
+// {demoExt: {text: 'an extension'}}
+// <message xmlns="jabber:client"><demo xmlns="jxt">an extension</demo></message>
+```
 
 ## License
 
---
+++ test/test.js
@@ -0,0 +1,206 @@
+"use strict";
+
+var test = require('tape');
+var core = require('../lib/core');
+var helpers = require('../lib/helpers');
+var types = require('../lib/types');
+
+
+var JXT = core.define({
+    name: 'jxtTest',
+    namespace: 'test',
+    element: 'jxt',
+    topLevel: true,
+    fields: {
+        attribute: types.attribute('attr'),
+        boolAttribute: types.boolAttribute('boolattr'),
+        subAttribute: types.subAttribute('test', 'subattr', 'attr'),
+        subText: types.subText('test', 'sub'),
+        multiSubText: types.multiSubText('test', 'sub'),
+        subLangText: types.subLangText('test', 'sublang')
+    }
+});
+
+
+var SubJXT = core.define({
+    name: 'subJXT',
+    namespace: 'test',
+    element: 'subjxt',
+    fields: {
+        text: types.text()
+    }
+});
+
+
+core.extend(JXT, SubJXT);
+
+
+test('basic attribute', function (t) {
+    var xml = helpers.createElement('test', 'test');
+
+    helpers.setAttribute(xml, 'attr', 'foo');
+    var res = helpers.getAttribute(xml, 'attr');
+
+    t.equal(res, 'foo');
+    t.end();
+});
+
+test('basic boolAttribute', function (t) {
+    var xml = helpers.createElement('test', 'test');
+
+    helpers.setBoolAttribute(xml, 'boolattr', true);
+    var res = helpers.getBoolAttribute(xml, 'boolattr');
+
+    t.equal(res, true);
+    t.end();
+});
+
+test('basic subAttribute', function (t) {
+    var xml = helpers.createElement('test', 'test');
+
+    helpers.setSubAttribute(xml, 'test', 'sub', 'attr', 'foo');
+    var res = helpers.getSubAttribute(xml, 'test', 'sub', 'attr');
+
+    t.equal(res, 'foo');
+    t.end();
+});
+
+test('basic text', function (t) {
+    var xml = helpers.createElement('test', 'test');
+
+    helpers.setText(xml, 'foo');
+    var res = helpers.getText(xml);
+
+    t.equal(res, 'foo');
+    t.end();
+});
+
+test('basic subText', function (t) {
+    var xml = helpers.createElement('test', 'test');
+
+    helpers.setSubText(xml, 'test', 'sub', 'foo');
+    var res = helpers.getSubText(xml, 'test', 'sub');
+
+    t.equal(res, 'foo');
+    t.end();
+});
+
+test('basic multiSubText', function (t) {
+    var xml = helpers.createElement('test', 'test');
+
+    helpers.setMultiSubText(xml, 'test', 'sub', ['foo', 'bar']);
+    var res = helpers.getMultiSubText(xml, 'test', 'sub');
+
+    t.deepEqual(res, ['foo', 'bar']);
+    t.end();
+});
+
+test('basic subLangText', function (t) {
+    var xml = helpers.createElement('test', 'test');
+
+    helpers.setSubLangText(xml, 'test', 'sub', {'en': 'foo', 'sv': 'bar'});
+    var res = helpers.getSubLangText(xml, 'test', 'sub');
+
+    t.deepEqual(res, {'en': 'foo', 'sv': 'bar'});
+    t.end();
+});
+
+test('field attribute', function (t) {
+    var xml = new JXT();
+
+    xml.attribute = 'foo';
+    var res = xml.attribute;
+
+    t.equal(res, 'foo');
+    t.end();
+});
+
+test('field boolAttribute', function (t) {
+    var xml = new JXT();
+
+    xml.boolAttribute = true;
+    var res = xml.boolAttribute;
+
+    t.equal(res, true);
+    t.end();
+});
+
+test('field subAttribute', function (t) {
+    var xml = new JXT();
+
+    xml.subAttribute = 'foo';
+    var res = xml.subAttribute;
+
+    t.equal(res, 'foo');
+    t.end();
+});
+
+test('field text', function (t) {
+    var xml = new SubJXT();
+
+    xml.text = 'foo';
+    var res = xml.text;
+
+    t.equal(res, 'foo');
+    t.end();
+});
+
+test('field subText', function (t) {
+    var xml = new JXT();
+
+    xml.subText = 'foo';
+    var res = xml.subText;
+
+    t.equal(res, 'foo');
+    t.end();
+});
+
+test('field multiSubText', function (t) {
+    var xml = new JXT();
+
+    xml.multiSubText = ['foo', 'bar'];
+    var res = xml.multiSubText;
+
+    t.deepEqual(res, ['foo', 'bar']);
+    t.end();
+});
+
+test('field subLangText', function (t) {
+    var xml = new JXT();
+
+    xml.subLangText = {'en': 'foo', 'sv': 'bar'};
+    var res = xml.subLangText;
+
+    t.deepEqual(res, {'en': 'foo', 'sv': 'bar'});
+    t.end();
+});
+
+test('extending', function (t) {
+    var xml = new JXT();
+
+    xml.subJXT.text = 'foo';
+    var res = xml.subJXT.text;
+
+    t.equal(res, 'foo');
+    t.end();
+});
+
+test('json', function (t) {
+    t.plan(2);
+
+    var xml = new JXT();
+
+    xml.attribute = 'foo';
+    xml.subJXT.text = 'bar';
+
+    var res = xml.toJSON();
+
+    t.deepEqual(res, {attribute: 'foo', subJXT: {text: 'bar'}});
+
+    var xml2 = new JXT(res);
+    var res2 = xml2.toJSON();
+
+    t.deepEqual(res2, {attribute: 'foo', subJXT: {text: 'bar'}});
+
+    t.end();
+});
