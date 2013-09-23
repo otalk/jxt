5fd1a80bda87d98d4cda7f0902c5ebe59b4f3f5b
Lance Stout <lancestout@gmail.com>
2013-09-23 13:51:24 -0700

    Add support a custom init call in constructor.

--- jxt.bundle.js
+++ jxt.bundle.js
@@ -9,7 +9,166 @@ var types = require('./lib/types');
 
 module.exports = _.extend({}, core, helpers, types);
 
-},{"./lib/core":2,"./lib/helpers":3,"./lib/types":4,"lodash":5}],5:[function(require,module,exports){
+},{"./lib/core":2,"./lib/helpers":3,"./lib/types":4,"lodash":5}],2:[function(require,module,exports){
+"use strict";
+
+var _ = require('lodash');
+var find = require('./helpers').find;
+var types = require('./types');
+
+var serializer = new XMLSerializer();
+
+var LOOKUP = {};
+var LOOKUP_EXT = {};
+var TOP_LEVEL_LOOKUP = {};
+
+
+function topLevel(JXT) {
+    var name = JXT.prototype._NS + '|' + JXT.prototype._EL;
+    LOOKUP[name] = JXT;
+    TOP_LEVEL_LOOKUP[name] = JXT;
+}
+
+function toString(xml) {
+    return serializer.serializeToString(xml);
+}
+
+function toJSON(jxt) {
+    var prop;
+    var result = {};
+    var exclude = {
+        constructor: true,
+        _EL: true,
+        _NS: true,
+        _extensions: true,
+        _name: true,
+        parent: true,
+        prototype: true,
+        toJSON: true,
+        toString: true,
+        xml: true
+    };
+
+    for (prop in jxt._extensions) {
+        if (jxt._extensions[prop].toJSON) {
+            result[prop] = jxt._extensions[prop].toJSON();
+        }
+    }
+
+    for (prop in jxt) {
+        if (!exclude[prop] && !((LOOKUP_EXT[jxt._NS + '|' + jxt._EL] || {})[prop]) && !jxt._extensions[prop] && prop[0] !== '_') {
+            var val = jxt[prop];
+            if (typeof val == 'function') continue;
+            var type = Object.prototype.toString.call(val);
+            if (type.indexOf('Object') >= 0) {
+                if (Object.keys(val).length > 0) {
+                    result[prop] = val;
+                }
+            } else if (type.indexOf('Array') >= 0) {
+                if (val.length > 0) {
+                    result[prop] = val;
+                }
+            } else if (!!val) {
+                result[prop] = val;
+            }
+        }
+    }
+
+    return result;
+}
+
+
+exports.build = function (xml) {
+    var JXT = TOP_LEVEL_LOOKUP[xml.namespaceURI + '|' + xml.localName];
+    if (JXT) {
+        return new JXT(null, xml);
+    }
+};
+
+exports.extend = function (ParentJXT, ChildJXT, multiName) {
+    var parentName = ParentJXT.prototype._NS + '|' + ParentJXT.prototype._EL;
+    var name = ChildJXT.prototype._name;
+    var qName = ChildJXT.prototype._NS + '|' + ChildJXT.prototype._EL;
+
+    LOOKUP[qName] = ChildJXT;
+    if (!LOOKUP_EXT[qName]) {
+        LOOKUP_EXT[qName] = {};
+    }
+    if (!LOOKUP_EXT[parentName]) {
+        LOOKUP_EXT[parentName] = {};
+    }
+    LOOKUP_EXT[parentName][name] = ChildJXT;
+
+    exports.add(ParentJXT, name, types.extension(ChildJXT));
+    if (multiName) {
+        exports.add(ParentJXT, multiName, types.multiExtension(ChildJXT));
+    }
+};
+
+exports.add = function (ParentJXT, fieldName, field) {
+    if (field.get) {
+        ParentJXT.prototype.__defineGetter__(fieldName, field.get);
+    }
+    if (field.set) {
+        ParentJXT.prototype.__defineSetter__(fieldName, field.set);
+    }
+};
+
+exports.define = function (opts) {
+    var StanzaConstructor = function (data, xml) {
+        var self = this;
+
+        self.xml = xml || document.createElementNS(self._NS, self._EL);
+        if (!self.xml.parentNode || self.xml.parentNode.namespaceURI !== self._NS) {
+            self.xml.setAttribute('xmlns', self._NS);
+        }
+
+        self._extensions = {};
+
+        _.each(self.xml.childNodes, function (child) {
+            var childName = child.namespaceURI + '|' + child.localName;
+            var ChildJXT = LOOKUP[childName];
+            if (ChildJXT !== undefined) {
+                var name = ChildJXT.prototype._name;
+                self._extensions[name] = new ChildJXT(null, child);
+                self._extensions[name].parent = self;
+            }
+        });
+
+        _.extend(self, data);
+
+        if (opts.init) {
+            opts.init(data);
+        }
+
+        return self;
+    };
+
+    StanzaConstructor.prototype = {
+        constructor: {
+            value: StanzaConstructor
+        },
+        _name: opts.name,
+        _eventname: opts.eventName,
+        _NS: opts.namespace,
+        _EL: opts.element,
+        toString: function () { return toString(this.xml); },
+        toJSON: function () { return toJSON(this); }
+    };
+
+    var fieldNames = Object.keys(opts.fields || {});
+    fieldNames.forEach(function (fieldName) {
+        exports.add(StanzaConstructor, fieldName, opts.fields[fieldName]);
+    });
+
+    if (opts.topLevel) {
+        topLevel(StanzaConstructor);
+    }
+
+    return StanzaConstructor;
+};
+
+},{"./helpers":3,"./types":4,"lodash":5}],5:[function(require,module,exports){
 (function(global){/**
  * @license
  * Lo-Dash 1.3.1 (Custom Build) <http://lodash.com/>
@@ -5567,162 +5726,98 @@ module.exports = _.extend({}, core, helpers, types);
 }(this));
 
 })(window)
-},{}],2:[function(require,module,exports){
+},{}],4:[function(require,module,exports){
 "use strict";
 
 var _ = require('lodash');
-var find = require('./helpers').find;
-var types = require('./types');
-
-var serializer = new XMLSerializer();
-
-var LOOKUP = {};
-var LOOKUP_EXT = {};
-var TOP_LEVEL_LOOKUP = {};
+var helpers = require('./helpers');
+var find = helpers.find;
 
 
-function topLevel(JXT) {
-    var name = JXT.prototype._NS + '|' + JXT.prototype._EL;
-    LOOKUP[name] = JXT;
-    TOP_LEVEL_LOOKUP[name] = JXT;
-}
-
-function toString(xml) {
-    return serializer.serializeToString(xml);
-}
-
-function toJSON(jxt) {
-    var prop;
-    var result = {};
-    var exclude = {
-        constructor: true,
-        _EL: true,
-        _NS: true,
-        _extensions: true,
-        _name: true,
-        parent: true,
-        prototype: true,
-        toJSON: true,
-        toString: true,
-        xml: true
-    };
-
-    for (prop in jxt._extensions) {
-        if (jxt._extensions[prop].toJSON) {
-            result[prop] = jxt._extensions[prop].toJSON();
-        }
-    }
-
-    for (prop in jxt) {
-        if (!exclude[prop] && !((LOOKUP_EXT[jxt._NS + '|' + jxt._EL] || {})[prop]) && !jxt._extensions[prop] && prop[0] !== '_') {
-            var val = jxt[prop];
-            if (typeof val == 'function') continue;
-            var type = Object.prototype.toString.call(val);
-            if (type.indexOf('Object') >= 0) {
-                if (Object.keys(val).length > 0) {
-                    result[prop] = val;
-                }
-            } else if (type.indexOf('Array') >= 0) {
-                if (val.length > 0) {
-                    result[prop] = val;
-                }
-            } else if (!!val) {
-                result[prop] = val;
+var field = exports.field = function (getter, setter) {
+    return function () {
+        var args = _.toArray(arguments);
+        return {
+            get: function () {
+                return getter.apply(null, [this.xml].concat(args));
+            },
+            set: function (value) {
+                setter.apply(null, ([this.xml].concat(args)).concat([value]));
             }
-        }
-    }
-
-    return result;
-}
-
-
-exports.build = function (xml) {
-    var JXT = TOP_LEVEL_LOOKUP[xml.namespaceURI + '|' + xml.localName];
-    if (JXT) {
-        return new JXT(null, xml);
-    }
-};
-
-exports.extend = function (ParentJXT, ChildJXT, multiName) {
-    var parentName = ParentJXT.prototype._NS + '|' + ParentJXT.prototype._EL;
-    var name = ChildJXT.prototype._name;
-    var qName = ChildJXT.prototype._NS + '|' + ChildJXT.prototype._EL;
-
-    LOOKUP[qName] = ChildJXT;
-    if (!LOOKUP_EXT[qName]) {
-        LOOKUP_EXT[qName] = {};
-    }
-    if (!LOOKUP_EXT[parentName]) {
-        LOOKUP_EXT[parentName] = {};
-    }
-    LOOKUP_EXT[parentName][name] = ChildJXT;
-
-    exports.add(ParentJXT, name, types.extension(ChildJXT));
-    if (multiName) {
-        exports.add(ParentJXT, multiName, types.multiExtension(ChildJXT));
-    }
-};
-
-exports.add = function (ParentJXT, fieldName, field) {
-    if (field.get) {
-        ParentJXT.prototype.__defineGetter__(fieldName, field.get);
-    }
-    if (field.set) {
-        ParentJXT.prototype.__defineSetter__(fieldName, field.set);
-    }
-};
-
-exports.define = function (opts) {
-    var StanzaConstructor = function (data, xml) {
-        var self = this;
-
-        self.xml = xml || document.createElementNS(self._NS, self._EL);
-        if (!self.xml.parentNode || self.xml.parentNode.namespaceURI !== self._NS) {
-            self.xml.setAttribute('xmlns', self._NS);
-        }
-
-        self._extensions = {};
-
-        _.each(self.xml.childNodes, function (child) {
-            var childName = child.namespaceURI + '|' + child.localName;
-            var ChildJXT = LOOKUP[childName];
-            if (ChildJXT !== undefined) {
-                var name = ChildJXT.prototype._name;
-                self._extensions[name] = new ChildJXT(null, child);
-                self._extensions[name].parent = self;
-            }
-        });
-
-        _.extend(self, data);
-
-        return self;
+        };
     };
+};
 
-    StanzaConstructor.prototype = {
-        constructor: {
-            value: StanzaConstructor
+exports.field = field;
+exports.attribute = field(helpers.getAttribute,
+                          helpers.setAttribute);
+exports.boolAttribute = field(helpers.getBoolAttribute,
+                              helpers.setBoolAttribute);
+exports.subAttribute = field(helpers.getSubAttribute,
+                             helpers.setSubAttribute);
+exports.text = field(helpers.getText,
+                     helpers.setText);
+exports.subText = field(helpers.getSubText,
+                        helpers.setSubText);
+exports.multiSubText = field(helpers.getMultiSubText,
+                             helpers.setMultiSubText);
+exports.subLangText = field(helpers.getSubLangText,
+                            helpers.setSubLangText);
+exports.boolSub = field(helpers.getBoolSub,
+                        helpers.setBoolSub);
+
+
+exports.extension = function (ChildJXT) {
+    return {
+        get: function () {
+            var name = ChildJXT.prototype._name;
+            if (!this._extensions[name]) {
+                var existing = find(this.xml, ChildJXT.prototype._NS, ChildJXT.prototype._EL);
+                if (!existing.length) {
+                    this._extensions[name] = new ChildJXT();
+                    this.xml.appendChild(this._extensions[name].xml);
+                } else {
+                    this._extensions[name] = new ChildJXT(null, existing[0]);
+                }
+                this._extensions[name].parent = this;
+            }
+            return this._extensions[name];
         },
-        _name: opts.name,
-        _eventname: opts.eventName,
-        _NS: opts.namespace,
-        _EL: opts.element,
-        toString: function () { return toString(this.xml); },
-        toJSON: function () { return toJSON(this); }
+        set: function (value) {
+            var child = this[ChildJXT.prototype._name];
+            _.extend(child, value);
+        }
     };
-
-    var fieldNames = Object.keys(opts.fields || {});
-    fieldNames.forEach(function (fieldName) {
-        exports.add(StanzaConstructor, fieldName, opts.fields[fieldName]);
-    });
-
-    if (opts.topLevel) {
-        topLevel(StanzaConstructor);
-    }
-
-    return StanzaConstructor;
 };
 
-},{"./helpers":3,"./types":4,"lodash":5}],3:[function(require,module,exports){
+exports.multiExtension = function (ChildJXT) {
+    return {
+        get: function () {
+            var data = find(this.xml, ChildJXT.prototype._NS, ChildJXT.prototype._EL);
+            var results = [];
+
+            _.forEach(data, function (xml) {
+                results.push(new ChildJXT({}, xml).toJSON());
+            });
+            return results;
+        },
+        set: function (value) {
+            var self = this;
+            var existing = find(this.xml, ChildJXT.prototype._NS, ChildJXT.prototype._EL);
+
+            _.forEach(existing, function (item) {
+                self.xml.removeChild(item);
+            });
+
+            _.forEach(value, function (data) {
+                var content = new ChildJXT(data);
+                self.xml.appendChild(content.xml);
+            });
+        }
+    };
+};
+
+},{"./helpers":3,"lodash":5}],3:[function(require,module,exports){
 "use strict";
 
 var _ = require('lodash');
@@ -5961,97 +6056,6 @@ exports.setBoolSub = function (xml, NS, element, value) {
     }
 };
 
-},{"lodash":5}],4:[function(require,module,exports){
-"use strict";
-
-var _ = require('lodash');
-var helpers = require('./helpers');
-var find = helpers.find;
-
-
-var field = exports.field = function (getter, setter) {
-    return function () {
-        var args = _.toArray(arguments);
-        return {
-            get: function () {
-                return getter.apply(null, [this.xml].concat(args));
-            },
-            set: function (value) {
-                setter.apply(null, ([this.xml].concat(args)).concat([value]));
-            }
-        };
-    };
-};
-
-exports.field = field;
-exports.attribute = field(helpers.getAttribute,
-                          helpers.setAttribute);
-exports.boolAttribute = field(helpers.getBoolAttribute,
-                              helpers.setBoolAttribute);
-exports.subAttribute = field(helpers.getSubAttribute,
-                             helpers.setSubAttribute);
-exports.text = field(helpers.getText,
-                     helpers.setText);
-exports.subText = field(helpers.getSubText,
-                        helpers.setSubText);
-exports.multiSubText = field(helpers.getMultiSubText,
-                             helpers.setMultiSubText);
-exports.subLangText = field(helpers.getSubLangText,
-                            helpers.setSubLangText);
-exports.boolSub = field(helpers.getBoolSub,
-                        helpers.setBoolSub);
-
-
-exports.extension = function (ChildJXT) {
-    return {
-        get: function () {
-            var name = ChildJXT.prototype._name;
-            if (!this._extensions[name]) {
-                var existing = find(this.xml, ChildJXT.prototype._NS, ChildJXT.prototype._EL);
-                if (!existing.length) {
-                    this._extensions[name] = new ChildJXT();
-                    this.xml.appendChild(this._extensions[name].xml);
-                } else {
-                    this._extensions[name] = new ChildJXT(null, existing[0]);
-                }
-                this._extensions[name].parent = this;
-            }
-            return this._extensions[name];
-        },
-        set: function (value) {
-            var child = this[ChildJXT.prototype._name];
-            _.extend(child, value);
-        }
-    };
-};
-
-exports.multiExtension = function (ChildJXT) {
-    return {
-        get: function () {
-            var data = find(this.xml, ChildJXT.prototype._NS, ChildJXT.prototype._EL);
-            var results = [];
-
-            _.forEach(data, function (xml) {
-                results.push(new ChildJXT({}, xml).toJSON());
-            });
-            return results;
-        },
-        set: function (value) {
-            var self = this;
-            var existing = find(this.xml, ChildJXT.prototype._NS, ChildJXT.prototype._EL);
-
-            _.forEach(existing, function (item) {
-                self.xml.removeChild(item);
-            });
-
-            _.forEach(value, function (data) {
-                var content = new ChildJXT(data);
-                self.xml.appendChild(content.xml);
-            });
-        }
-    };
-};
-
-},{"./helpers":3,"lodash":5}]},{},[1])(1)
+},{"lodash":5}]},{},[1])(1)
 });
 ;
